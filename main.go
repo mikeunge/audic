9420bad84118eb6ff21a405aa95f63dbd2ab88ec
mikeunge <mikeunge@protonmail.com>
2021-04-18 13:53:32 +0200

    [add] check if a command exists before exection, spawn a pavucontrol gui

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 const defaultVolume = 10
 const appName = "audio-controller"
 const appNameShort = "audic"
-const version = "0.1.1"
+const version = "0.1.2"
 
 type audio struct {
 	Action  string
@@ -30,11 +30,38 @@ func help() {
 	fmt.Println("  -    set N\t Set the volume to N percent")
 	fmt.Println("  - volume\t Show the current volume")
 	fmt.Println("  -   mute\t Mute/Unmute the audio")
+	fmt.Println("  -   gui\t Spawn a GUI via pavucontrol")
 	fmt.Println("  -   help\t Show this message and exit")
+}
+
+// cmdExists - make sure a command exists before executing it
+func cmdExists(cmdlet string) error {
+	cmd := exec.Command("which", cmdlet)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf(fmt.Sprint(err) + ": " + stderr.String())
+	}
+	return nil
+}
+
+// inRange - make sure that the next element is in range before assigning it.
+func inRange(min int, max int) bool {
+	if min < max {
+		return true
+	}
+	return false
 }
 
 // volume - get the current volume
 func volume() (int, error) {
+	err := cmdExists("pactl")
+	if err != nil {
+		return -1, fmt.Errorf("command 'pactl' does not exist")
+	}
 	// this is a weird way of executing a command but it's the only way this pipe works
 	command := "pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,'"
 	cmd := exec.Command("bash", "-c", command)
@@ -42,7 +69,7 @@ func volume() (int, error) {
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return -1, fmt.Errorf(fmt.Sprint(err) + ": " + stderr.String())
 	}
@@ -59,26 +86,42 @@ func changeVolume(ad audio) error {
 	percent := strconv.Itoa(ad.Percent) + "%"
 	switch ad.Action {
 	case "increase":
+		err := cmdExists("pactl")
+		if err != nil {
+			return fmt.Errorf("command 'pactl' does not exist")
+		}
 		cmd := exec.Command("pactl", "--", "set-sink-volume", "0", "+"+percent)
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			return err
 		}
 	case "decrease":
+		err := cmdExists("pactl")
+		if err != nil {
+			return fmt.Errorf("command 'pactl' does not exist")
+		}
 		cmd := exec.Command("pactl", "--", "set-sink-volume", "0", "-"+percent)
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			return err
 		}
 	case "set":
+		err := cmdExists("pactl")
+		if err != nil {
+			return fmt.Errorf("command 'pactl' does not exist")
+		}
 		cmd := exec.Command("pactl", "--", "set-sink-volume", "0", percent)
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			return err
 		}
 	case "mute":
+		err := cmdExists("pactl")
+		if err != nil {
+			return fmt.Errorf("command 'pactl' does not exist")
+		}
 		cmd := exec.Command("pactl", "--", "set-sink-mute", "0", "toggle")
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			return err
 		}
@@ -88,16 +131,19 @@ func changeVolume(ad audio) error {
 			return err
 		}
 		fmt.Printf("Volume: %d\n", vol)
+	case "gui":
+		err := cmdExists("pavucontrol")
+		if err != nil {
+			return fmt.Errorf("command 'pavucontrol' does not exist")
+		}
+		cmd := exec.Command("pavucontrol")
+		fmt.Println("Press Ctrl+C to exit pavucontrol")
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
-}
-
-// inRange - make sure that the next element is in range before assigning it.
-func inRange(min int, max int) bool {
-	if min < max {
-		return true
-	}
-	return false
 }
 
 // argparser - parse the provided arguments
@@ -144,6 +190,8 @@ func argparser() (audio, error) {
 		ad.Action = "mute"
 	case "volume", "--volume", "-v":
 		ad.Action = "volume"
+	case "gui", "--gui", "-g":
+		ad.Action = "gui"
 	case "help", "--help", "-h":
 		help()
 		os.Exit(0)
