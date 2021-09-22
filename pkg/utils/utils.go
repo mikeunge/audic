332dc9731e7866541804ef6d5932ada480a24d68
Mike Unge <mikeunge@protonmail.com>
2021-09-22 17:56:08 +0200

    chg: got rid of the 'InRange' fucntion because it's obsolete since we changed to the new argparser, we also change the stdout & stderr back to there origin after we tested if a program exists or not

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,33 +1,34 @@
 package utils
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 	"os/exec"
 )
 
 // CmdExists - make sure a command exists before executing it
 func CmdExists(cmdlet string) error {
-	cmd := exec.Command("which", cmdlet)	// we use `which` for the probing
-	var out bytes.Buffer
+	cmd := exec.Command("which", cmdlet) // we use `which` for the probing
+	var stdout bytes.Buffer
 	var stderr bytes.Buffer
+
+	// capture old stdout & stderr
+	oldStdout := cmd.Stdout
+	oldStderr := cmd.Stderr
+
 	// redirect the stdout and stderr to the created bytebuffer.
 	// this ensures the user doesn't see anything from the command probing.
-	cmd.Stdout = &out
+	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
+
 	// run the command and check for errors.
 	err := cmd.Run()
+
+	// Switch Stdout & Stderr back
+	cmd.Stdout = oldStdout
+	cmd.Stderr = oldStderr
 	if err != nil {
-		fmt.Println("")
 		return fmt.Errorf(fmt.Sprint(err) + ": " + stderr.String())
 	}
 	return nil
-}
-
-// InRange - make sure that the next element is in range before assigning it.
-func InRange(min int, max int) bool {
-	if min < max {
-		return true
-	}
-	return false
 }
