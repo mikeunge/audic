f4966384eb355172fa4583284c1999ed5026bd90
Mike Unge <mikeunge@protonmail.com>
2021-09-22 17:58:37 +0200

    chg: refactored the parser (s -> settigns), we also set some settings before we launch the parser (eg. settings.SinkPath

--- main.go
+++ main.go
@@ -6,59 +6,61 @@ import (
 
 	"github.com/akamensky/argparse"
 
-	audio "github.com/mikeunge/audic/pkg/audio"
+	"github.com/mikeunge/audic/pkg/audio"
 )
 
-// Specify the default increse/decrease volume.
-const defaultVolume = 10
-const appName = "audic"
-const version = "0.1.3.2"
+const (
+	appName = "audic"
+	version = "0.1.3.2"
+)
 
-func parser(s *audio.Settings) error {
-	parser := argparse.NewParser(appName, "The easiest way to control your audio.")
+func parser(settings *audio.Settings) error {
+	parser := argparse.NewParser(appName+" - "+version, "The easiest way to control your audio.")
 
 	// Parser options
 	volume := parser.NewCommand("volume", "Set/Increase/Decrease the volume")
-	sinkV := volume.Int("S", "sink", &argparse.Options{Required: false, Help: "Set the sink you want to control", Default: 0})
+	sinkV := volume.Int("S", "sink", &argparse.Options{Required: false, Help: "Set the sink you want to control", Default: -1})
 	increase := volume.Int("i", "increase", &argparse.Options{Required: false, Help: "Increase the volume by N"})
 	decrease := volume.Int("d", "decrease", &argparse.Options{Required: false, Help: "Decrease the volume by N"})
 	set := volume.Int("s", "set", &argparse.Options{Required: false, Help: "Set the volume to N"})
-	show := volume.Flag("m", "show", &argparse.Options{Required: false, Help: "Show the volume eg. Volume: 80%"})
+	show := volume.Flag("m", "show", &argparse.Options{Required: false, Help: "Show the volume"})
 	toggle := parser.NewCommand("toggle", "Toggle the audio (mute/unmute)")
-	sinkT := toggle.Int("S", "sink", &argparse.Options{Required: false, Help: "Set the sink you want to control", Default: 0})
+	sinkT := toggle.Int("S", "sink", &argparse.Options{Required: false, Help: "Set the sink you want to control", Default: -1})
 	gui := parser.NewCommand("gui", "Open a GUI (requires pavucontrol)")
 
-	// Parse the options
+	// Parse the input
 	err := parser.Parse(os.Args)
 	if err != nil {
-		fmt.Print(parser.Usage(""))
+		fmt.Print(parser.Usage("")) // print help
 		return fmt.Errorf("parser returned with an error, no arguments provided")
 	}
 
 	if toggle.Happened() {
-		s.Action = "toggle"
-		s.Sink = *sinkT
+		settings.Action = "toggle"
+		audio.SinkVal(sinkT, settings)
 		return nil
 	}
 
 	if gui.Happened() {
-		s.Action = "gui"
+		settings.Action = "gui"
 		return nil
 	}
 
+	// parse if volume command was set
+	// check for all the subcommands and their values
 	if volume.Happened() {
-		s.Sink = *sinkV
-		if (*increase != 0) {
-			s.Action = "increase"
-			s.Percent = *increase
-		} else if (*decrease != 0) {
-			s.Action = "decrease"
-			s.Percent = *decrease
-		} else if (*set != 0) {
-			s.Action = "set"
-			s.Percent = *set
+		audio.SinkVal(sinkV, settings)
+		if *increase != 0 {
+			settings.Action = "increase"
+			settings.Percent = *increase
+		} else if *decrease != 0 {
+			settings.Action = "decrease"
+			settings.Percent = *decrease
+		} else if *set != 0 {
+			settings.Action = "set"
+			settings.Percent = *set
 		} else if *show {
-			s.Action = "show"
+			settings.Action = "show"
 		} else {
 			return fmt.Errorf("no subcommand provided, use -h/--help for more information")
 		}
@@ -66,20 +68,30 @@ func parser(s *audio.Settings) error {
 	return nil
 }
 
-
 func main() {
-	var s audio.Settings
+	var settings audio.Settings
+	settings.SinkPath = "/tmp/audic.sink" // where the sink cache is located
+	settings.SetSink = false              // trigger, if we should write to cache (will get changed later)
 
-	err := parser(&s)
+	// parse the provided arguments
+	err := parser(&settings)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	err = audio.Controller(&s)
+	// pass the instructions to the controller
+	err = audio.Controller(&settings)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		os.Exit(1)
+	}
+
+	// write the sink to cache
+	if settings.SetSink {
+		// we really don't care if it failed or not
+		// this is only interesting when we debug the program
+		_ = audio.WriteSink(&settings)
 	}
 	os.Exit(0)
 }
